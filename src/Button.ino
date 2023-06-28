be8a7625e314901151eca6eb84527538560c6026
Fenton Pratt <fadpratt@gmail.com>
2023-06-28 08:55:43 -0500

    Starting Comments Gone

--- src/Button.ino
+++ src/Button.ino
@@ -1,24 +1,16 @@
-/*
- * Project Button
- * Description:
- * Author:
- * Date:
- */
 SYSTEM_MODE(MANUAL);
 SYSTEM_THREAD(ENABLED);
-// setup() runs once, when the device is first turned on.
+
 void setup() {
-  // Put initialization like pinMode and begin functions here.
   pinMode(D0, INPUT);
   pinMode(D1, OUTPUT);
   Serial.begin(9600);
 }
 
-// loop() runs over and over again, as quickly as it can execute.
 bool once = false;
 bool state = false;
+
 void loop() {
-  // The core of your code will likely live here.
   if(digitalRead(D0)==HIGH&&!once) {
     Serial.println("Button Clicked");
     state = !state;
